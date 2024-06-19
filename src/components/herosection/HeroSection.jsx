9d902e4108150dc856406f627383ffcebee8f06d
rabby-shek <rabbyshek1998@gmail.com>
2024-06-19 13:39:57 +0600

    initial push

---
+++ src/components/herosection/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='hero-section '>
+      hero section
+      <span className="circle-1 "></span>
+      <span className="circle-2 "></span>
+      <span className="circle-3 "></span>
+      <span className="circle-4 "></span>
+      <span className="circle-5 "></span>
+    </div>
+  )
+}
+
+export default HeroSection;
